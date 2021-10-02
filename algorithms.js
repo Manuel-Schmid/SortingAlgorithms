5d45701c65bbce78609ea9e580840a6dadc36c4d
Manuel Schmid <manysch3@gmail.com>
2021-10-02 22:24:54 +0200

    added working mergeSort algorithm (without visualisation)

--- algorithms.js
+++ algorithms.js
@@ -75,6 +75,15 @@ function delay(delayInMs) {
     });
 }
 
+function printNumsObjToChart(numsObj) {
+    let bars = document.getElementsByClassName("bar");
+    for (let i = 0; i < bars.length; i++) {
+        if (numsObj[i] !== undefined) {
+            bars[i].style.height = numsObj[i] + "px";
+        }
+    }
+}
+
 function printToChart(numbers) {
     let bars = document.getElementsByClassName("bar");
     for (let i = 0; i < bars.length; i++) {
@@ -87,3 +96,76 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function initMergeSort() {
+    let numsObj = {}
+    for (let i = 0; i < nums.length; i++) {
+        numsObj[i] = nums[i]
+    }
+    // console.log(Object.size(numsObj))
+    console.log(numsObj)
+    // console.log(getPartOfNumsObj(Object.keys(numsObj).filter(key => (key < nums.length/2)), numsObj))
+    // console.log(getPartOfNumsObj(Object.keys(numsObj).filter(key => (key >= nums.length/2)), numsObj))
+    printNumsObjToChart(mergeSort(numsObj))
+}
+
+function getPartOfNumsObj(start, stop, numsObj) {
+    let newNumsObj = {}
+
+    index = start
+    for(var prop in numsObj) {
+        if (index >= stop) break
+        else {
+            newNumsObj[prop] = numsObj[index]
+            index++
+        }
+    }
+    return newNumsObj
+}
+
+function mergeSort(numsObj) {
+    if (Object.size(numsObj) <= 1) {
+        return numsObj
+    }
+    console.log(Object.size(numsObj))
+
+    // const keys = Object.keys(numsObj)
+    const left = getPartOfNumsObj(0, Object.size(numsObj)/2, numsObj)
+    const right = getPartOfNumsObj(Object.size(numsObj)/2, Object.size(numsObj), numsObj)
+    console.log(left, right)
+
+    const res = merge(mergeSort(left), mergeSort(right));
+    return res
+}
+
+function merge(leftObj, rightObj) {
+    let sortedArr = [];
+    let left = Object.values(leftObj)
+    let right = Object.values(rightObj)
+
+    while (left.length && right.length) {
+        // insert the smallest element to the sortedArr
+        if (left[0] < right[0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
+        }
+    }
+
+    const newArr = [...sortedArr, ...left, ...right];
+
+    let numsObj = {}
+    for (let i = 0; i < newArr.length; i++) {
+        numsObj[i] = newArr[i]
+    }
+    return numsObj
+}
+
+Object.size = function(obj) {
+    var size = 0,
+        key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
